d5f1bc83f90a3644413e9c47e1df4ff824b25c58
Brad Forsyth <b4syth@gmail.com>
2016-07-25 14:03:48 -0600

    Created PostTableViewCell class

---
+++ Timeline/PostTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostTableViewCell.swift
+//  Timeline
+//
+//  Created by Brad on 7/25/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var postImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func updateWithPost(post: Post) {
+        guard let imageData = post.photoData, image: UIImage = UIImage(data: imageData) else { return }
+        postImageView.image = image
+    }
+}
